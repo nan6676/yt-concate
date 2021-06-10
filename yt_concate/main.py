388e4c9cf3f0c8bf6c149ff91e0eb3a60aba9e98
nan6676 <nan6676@gmail.com>
2021-06-10 23:14:34 +0800

    Successfully read captions into data structure and fixed step class

--- yt_concate/main.py
+++ yt_concate/main.py
@@ -1,7 +1,8 @@
 from yt_concate.pipeline.pipeline import Pipeline
+from yt_concate.pipeline.steps.preflight import Preflight
 from yt_concate.pipeline.steps.download_captions import DownloadCaptions
 from yt_concate.pipeline.steps.get_video_list import GetVideoList
-from yt_concate.pipeline.steps.preflight import Preflight
+from yt_concate.pipeline.steps.read_caption import ReadCaption
 from yt_concate.pipeline.steps.postflight import Postflight
 from yt_concate.utils import Utils
 
@@ -15,6 +16,7 @@ def main():
         Preflight(),
         GetVideoList(),
         DownloadCaptions(),
+        ReadCaption(),
         Postflight(),
     ]
 
